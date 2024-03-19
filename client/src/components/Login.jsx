2260bfd479f04e40dc8de0a0b6f8d36c7fa6af52
Kritika Walia <kritikawalia987@gmail.com>
2024-03-19 15:38:14 +0530

    Added Login form

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,39 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Login() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Password:", password);
+    
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
+
   return (
     <>
-    <form>
-    <div className='login'>
-        <h3>Login to your account</h3>
-      <div>
-      <label htmlFor="name"> Username</label>
-      <input type="text" />
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div className='login'>
+          <h3>Login to your account</h3>
+          <div>
+            <label htmlFor="name">Username</label>
+            <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+          </div>
 
-      <div>
-      <label htmlFor="email">Email</label>
-      <input type="email"/>
-      </div>
-      
-      <div>
-      <label htmlFor="password">Password</label>
-      <input type="password" />
-      </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
 
-      <div>
-      <button>Login</button>
-      </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
 
-      <div>
-        <h4>Don't have an account? <Link to='/signup' style={{ textDecoration: 'none', color: 'blue' }}>Sign Up</Link></h4>
-      </div>
-      
-    </div>
-    </form>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+
+          <div>
+            <h4>Don't have an account? <Link to='/signup' style={{ textDecoration: 'none', color: 'blue' }}>Sign Up</Link></h4>
+          </div>
+        </div>
+      </form>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
