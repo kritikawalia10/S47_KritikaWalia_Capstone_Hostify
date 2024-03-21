7afe0ed46bddd1d98901284214a486786dcd2b09
Kritika Walia <kritikawalia987@gmail.com>
2024-03-21 16:46:40 +0530

    Fetching data

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -1,25 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function MainPage() {
+  const apiUrl = 'http://localhost:8080/';
+  const [data, setData] = useState([]);
+  const [originalData, setOriginalData] = useState([]);
+
+
+  const filterData = (event) => {
+    const query = event.target.value;
+    if (query.trim() === '') {
+      setData(originalData); 
+      return;
+    }
+    const filteredData = originalData.filter((item) => {
+      return item.hostelName.toLowerCase().includes(query.toLowerCase());
+    });
+    setData(filteredData);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(apiUrl);
+        setData(response.data);
+        setOriginalData(response.data); 
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, [apiUrl]);
+
   return (
     <>
-    <div className="main">
+      <div className="main">
         <div className="bar">
-            <div className="head">
+          <div className="head">
             <h3>Account</h3>
             <h3>Language</h3>
             <h3>Help</h3>
             <h3>Features</h3>
             <h3>Facilities</h3>
-            </div>
-
+          </div>
         </div>
-      <input type="text" placeholder='Search...' />
-
-
-    </div>
+        <div>
+          <input type="text" placeholder='Search...' onChange={filterData} />
+          {data && data.map((item, index) => {
+            return (
+              <div className="card" key={index}>
+                <div>
+                  <h3>Hostel : {item.hostelName}</h3>
+                  <p>Available Rooms : {item.roomsAvail}</p>
+                  <p>Room Type : {item.roomType}</p>
+                  <p>Price : {item.price}</p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </>
   )
 }
 
-export default MainPage
+export default MainPage;
