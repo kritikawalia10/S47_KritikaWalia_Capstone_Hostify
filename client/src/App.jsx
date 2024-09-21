00d9da9f5d8bd97cfd20a029365d58253728e9bb
Kritika Walia <kritikawalia987@gmail.com>
2024-09-21 11:22:15 +0530

    Used google auth Provider for authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
-import Home from './components/Home'
-import './App.css'
-import {Routes, Route} from 'react-router-dom'
-import About from './components/About'
-import Nav from './components/Nav'
-import Contact from './components/Contact'
-import Login from './components/Login'
-import SignUp from './components/SignUp'
-import MainPage from './components/MainPage'
-import Add from './components/Add'
-function App() {
+import React from 'react';
+import Home from './components/Home';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import About from './components/About';
+import Nav from './components/Nav';
+import Contact from './components/Contact';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import MainPage from './components/MainPage';
+import Add from './components/Add';
+import { GoogleOAuthProvider } from '@react-oauth/google'; // Import GoogleOAuthProvider
 
+function App() {
   return (
-    <> 
-      <Nav/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path = '/about' element={<About/>}/>
-        <Route path = '/contact' element={<Contact/>}/>
-        <Route path = '/login' element={<Login/>}/>
-        <Route path = '/signup' element={<SignUp/>}/>
-        <Route path = '/main' element={<MainPage/>}/>
-        <Route path = '/add' element={<Add/>}/>
-      </Routes>
-      
+    <>
+      <GoogleOAuthProvider clientId="70314281965-8io920bulao023rl36gpcdb2bk2lb20d.apps.googleusercontent.com"> {/* Wrap your app with the provider */}
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/main' element={<MainPage />} />
+          <Route path='/add' element={<Add />} />
+        </Routes>
+      </GoogleOAuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
